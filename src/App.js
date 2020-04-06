588d406b0fed38e65f587a3111e2213f074354cf
Monika Frankowska <monia.frankowska@gmail.com>
2020-04-06 16:20:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
 
     //Search Function
 
-    search = (query = 'cat') => {
+    search = (query = 'horse') => {
         axios.get(` https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
             .then(response => {
                 this.setState({
